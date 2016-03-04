19a5572658e90e735567e756f1c54b16f40e7df3
–Ь–Є—Е–∞–Є–ї –Ъ–ї–Є–Љ—З—Г–Ї <m-klimchuk@yandex.ru>
2016-03-04 22:38:50 +0300

    Initial Commit

---
+++ HSE_day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HSE_day
+//
+//  Created by mikhail klimchuk on 04.03.16.
+//  Copyright © 2016 mikhail klimchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
